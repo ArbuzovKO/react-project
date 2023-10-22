ddf86ced5884a13eb44882e5193fb1e1cbd5a574
Arbuzov Kirill <121497979+ArbuzovKO@users.noreply.github.com>
2023-10-22 22:01:35 +0300

    initial commit

--- index.js
+++
@@ -1,3 +0,0 @@
-const x = [1,2,3];
-const y = [4,5,6];
-console.log(x === y);
--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,28 @@
 import { Link, Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import './index.scss';
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import './styles/index.scss';
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+    
     return (
-        <div className="app">
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path={'/about'} element={<AboutPage />} />
-                <Route path={'/'} element={<MainPage />} />
+                <Route path={'/about'} element={<AboutPageAsync />} />
+                <Route path={'/'} element={<MainPageAsync />} />
             </Routes>
+            </Suspense>
         </div>
     );
 };
